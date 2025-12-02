0369298d279b1ac4ffec6476c04cfae118805953
Centrix14 <artiom.zvyagin@yandex.ru>
2025-12-02 23:04:46 +0300

    initial 118 lines of code

---
+++ avc.lisp
@@ -0,0 +1,118 @@
+;;;; todo:
+;;;; 1) выполнить все имеющиеся todo
+;;;; 2) добавить интерфейс с запуском (s), окончанием (e) и превалидацией (pv) - она должна обнаруживать неправильные записи и только
+
+(defparameter +municipality-types+ '("г" "пгт" "с" "д"))
+(defparameter +path-types+ '("ул" "б-р" "ш"))
+(defparameter +building-types+ '("д" "корп" "стр"))
+
+;;;; validation functions
+
+(defun validate (pattern addr)
+  "validates addr by given pattern"
+  (declare (type list pattern addr))
+
+  (when (> (length pattern) (length addr))
+    (return-from validate nil))
+  
+  (let ((i 0)
+        log arity matcher)
+    
+    (dolist (rule pattern)
+
+      (setf arity 1)
+
+      (cond
+        
+        ((symbolp rule) (setf matcher rule))
+
+        ((listp rule)
+         (unless (or (eql 'n (second rule)) ;; unless arity == n | integer
+                     (integerp (second rule)))
+           (error "Arity must be integer or 'n"))
+         
+         (setf matcher (first rule))
+         (setf arity (nil-as 0 (second rule))))
+
+        (t (error "Invalid pattern rule ~a" rule)))
+
+      (if (eql arity 'n) ;; todo: separate branches into %functions%
+          (do ()
+              ((or (= i (length addr))
+                   (not (funcall matcher (nth i addr)))))
+            (incf i))
+          (dotimes (n arity)
+            (unless (funcall matcher (nth i addr))
+              (push (list rule i (nth i addr)) log))
+            (incf i))))
+
+    (values (null log) log)))
+
+;;;; address parts predicates
+
+(defun anythingp (value)
+  "envelopes any readable Lisp value"
+  (declare (ignore value))
+  t)
+
+(defun post-index-p (value)
+  "returns T for post-indexes (6 digits)"
+  (declare (type string value))
+  
+  (loop for char across value do ;; todo: extract pattern into macro
+        (unless (digit-char-p char)
+          (return-from post-index-p nil)))
+  
+  (= 6 (length value)))
+
+(defun municipality-type-p (value)
+  "returns T for values from +municipality-types+"
+  (one-of-strings value +municipality-types+))
+
+(defun toponymp (value)
+  "returns T for values = [a-zA-Z0-9\- ]"
+  (declare (type string value))
+
+  (loop for char across value do
+    
+        (unless (or (alphanumericp char)
+                    (char= #\Space char)
+                    (char= #\- char))
+          (return-from toponymp nil)))
+  t)
+
+(defun path-type-p (value)
+  "returns T for values from +path-types+"
+  (one-of-strings value +path-types+))
+
+(defun building-type-p (value)
+  "returns T for values from +building-types+"
+  (one-of-strings value +building-types+))
+
+(defun building-number-p (value)
+  "returns T for values = [a-zA-Z0-9\/\- ]"
+  (declare (type string value))
+
+  (loop for char across value do
+
+        (unless (or (alphanumericp char)
+                    (char= #\/ char)
+                    (char= #\- char)
+                    (char= #\Space char))
+          (return-from building-number-p nil)))
+  t)
+
+;;;; utils
+
+(defun nil-as (alias value)
+  "interprets value as alias if value == nil"
+  (if value value alias))
+
+(defun as-logical (value)
+  "interprets nil and other values as nil and T"
+  (if value t nil))
+
+;; todo: rewrite into macro
+(defun one-of-strings (value list)
+  (declare (type string value))
+  (as-logical (member value list :test #'string=)))
